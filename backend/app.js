393ea0455c41c97a7136a0ff346e69f4da48a736
hernango <hernanhidelberg@gmail.com>
2021-10-01 13:14:10 -0500

    fusion desarrollo-produccion

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let express = require("express"),
   cors = require("cors"),
   mongoose = require("mongoose"),
@@ -22,60 +21,6 @@ mongoose
   );
 
 const studentAPI = require("../backend/routes/student.route");
-const app = express();
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-app.use(cors());
-
-// API
-app.use("/api", studentAPI);
-
-// Create port
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-  console.log("Connected to port " + port);
-});
-
-// Find 404
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
-});
-||||||| merged common ancestors
-=======
-let express = require("express"),
-  cors = require("cors"),
-  mongoose = require("mongoose"),
-  database = require("./database"),
-  bodyParser = require("body-parser");
-
-// Connect mongoDB
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(database.db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(
-    () => {
-      console.log("Database connected");
-    },
-    (error) => {
-      console.log("Database could't be connected to: " + error);
-    }
-  );
-
-//const studentAPI = require("../backend/routes/student.route");
 const usuarioAPI = require("../backend/routes/usuario.route");
 
 const app = express();
@@ -88,7 +33,7 @@ app.use(
 app.use(cors());
 
 // API
-//app.use("/api", studentAPI);
+app.use("/api", studentAPI);
 app.use("/usuario-servicios", usuarioAPI)
 
 // Create port
@@ -112,4 +57,3 @@ app.use(function(err, req, res) {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
 });
->>>>>>> DESARROLLO
